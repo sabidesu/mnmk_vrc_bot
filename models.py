512d45b1d8f6a673069306b186519f191c14bf2a
sabi <zentixzaneic@gmail.com>
2023-10-27 14:21:26 -0400

    fix model

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from basic_notion.page import NotionPage, NotionPageList
 from basic_notion.field import TitleField, SelectField, DateField
 
 class Movie(NotionPage):
-  title = TitleField()
-  status = SelectField(options=['watched', 'to watch'])
-  watch_date = DateField()
+  name = TitleField(property_name='name')
+  status = SelectField(property_name='status')
+  watch_date = DateField(property_name='watch date')
 
 class MovieList(NotionPageList[Movie]):
   ITEM_CLS = Movie
