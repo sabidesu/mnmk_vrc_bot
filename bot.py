b31a3bd2ea955ad76b8686177cfc5db2b550823f
sabi <zentixzaneic@gmail.com>
2023-11-08 14:49:16 -0500

    begin logging with bot

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+import logging
 
 from notion_client import Client
 from basic_notion.query import Query
@@ -8,8 +9,11 @@ from models import MovieList, Movie
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
+logging.basicConfig(filename='bot.log', encoding='utf-8', level=logging.INFO)
+
 @bot.message_handler(commands=['movierequest', 'mr'])
 def request_handler(message):
+  logging.info(f"request from {message.from_user.username}")
   text = "what movie do you want to request? format your request like this: *movie name (year)*\ni.e. *the matrix (1999)*\n\nnote: please write the full name of the movie! this is to avoid confusion with other movies with the same name.if you're unsure, use google :)\ni.e. if you want to watch _django unchained_, instead of *django*, write *django unchained*"
   sent_msg = bot.reply_to(message, text, parse_mode='Markdown')
   bot.register_next_step_handler(sent_msg, movie_response)
